1cec581d578ed8c6f38ba54026e925c6b93da900
user20100330 <134914842+user20100330@users.noreply.github.com>
2023-06-08 12:43:21 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function generate(){
-  let el = document.getElementById("display");
+  let el = document.querySelector(".number");
   const randmun = Math.floor(Math.random() * 37);
   el.innerHTML = randmun;
 }
