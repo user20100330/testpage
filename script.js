46ff7a226e9dde318ee457532aaaf7acd5106535
user20100330 <134914842+user20100330@users.noreply.github.com>
2023-06-08 13:10:58 +0900

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,10 @@ function generate(){
   let el = document.getElementById("number");
   const randmun = Math.floor(Math.random() * 37);
   el.innerHTML = randmun;
-  
+  let li = document.getElementById("list");
+  var newli = document.createElement('li');
+  newli.innerHTML = randmun;
+  li.appendChild(newli);
 }
 
 function arrayShuffle(array) {
