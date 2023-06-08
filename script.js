81ba1624d8b990dc232213865babd44a0bc7ee57
user20100330 <134914842+user20100330@users.noreply.github.com>
2023-06-08 13:07:35 +0900

    Update script.js

--- script.js
+++ script.js
@@ -2,4 +2,19 @@ function generate(){
   let el = document.getElementById("number");
   const randmun = Math.floor(Math.random() * 37);
   el.innerHTML = randmun;
+  
+}
+
+function arrayShuffle(array) {
+  for(let i = (array.length - 1); 0 < i; i--){
+
+    // 0〜(i+1)の範囲で値を取得
+    let r = Math.floor(Math.random() * (i + 1));
+
+    // 要素の並び替えを実行
+    let tmp = array[i];
+    array[i] = array[r];
+    array[r] = tmp;
+  }
+  return array;
 }
