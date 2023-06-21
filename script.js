bb0218127d5b6763797bd8fa9163957d0e1a85e5
user20100330 <134914842+user20100330@users.noreply.github.com>
2023-06-21 14:20:14 +0900

    Update script.js

--- script.js
+++ script.js
@@ -24,19 +24,15 @@ window.onload = function(){
   init() //ページロード時にリストを初期化
   let maxnum = document.getElementById("max");
   maxnum.addEventListener("input", function() {
-    if(isNaN(maxnum.value)){
-      maxnum.style.outline = "solid 2px red"
-      maxnum.innerHTML = "";
-    } else if(maxnum.value >= 1 && maxnum.value <= 99) {
-      maxnum.style.outline = "solid 2px blue"
-      let checks = document.getElementsByClassName("checks");
-      let nonum = document.getElementById("nonum");
-      let clone = nonum.cloneNode( false );
-      nonum.parentNode.replaceChild( clone , nonum );
-      if(checks.length !== maxnum.value){
-        for(i=1;i<(maxnum.value+1);i++){
-          createcheck(i);
-        }
+    if(maxnum.value >= 1 && maxnum.value <= 99) {
+    maxnum.style.outline = "solid 2px blue"
+    let checks = document.getElementsByClassName("checks");
+    let nonum = document.getElementById("nonum");
+    let clone = nonum.cloneNode( false );
+    nonum.parentNode.replaceChild( clone , nonum );
+    if(Number.isInteger(maxnum.value) && checks.length !== maxnum.value){
+      for(i=0;i<maxnum.value;i++){
+        createcheck(i+1);
       }
     } else {
       maxnum.style.outline = "solid 2px red"
