b689ecc142b3ad4b5f5c43ffb8c329ca584a6c40
user20100330 <134914842+user20100330@users.noreply.github.com>
2023-06-21 09:43:23 +0900

    Update script.js

--- script.js
+++ script.js
@@ -31,7 +31,7 @@ window.onload = function(){
       maxnum.style.outline = "solid 2px blue"
       let checks = document.getElementsByClassName('checks');
       let nonum = document.getElementById("nonum");
-      var clone = nonum.cloneNode( false );
+      let clone = nonum.cloneNode( false );
       nonum.parentNode.replaceChild( clone , nonum );
       if(checks.length !== maxnum.value){
         for(i=1;i<checks.length;i++){
